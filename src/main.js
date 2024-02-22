f29d14d08c75ae319a7c8c917659df504f049b82
Philani <mxathule99@gmail.com>
2024-02-22 18:39:31 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
+import bootstrap from 'bootstrap'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(bootstrap).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h6>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe deleniti cum tenetur totam nobis inventore ullam est, non ex quis commodi distinctio porro exercitationem, magnam earum a quasi velit. Expedita.
+      Eum excepturi voluptatum ipsa blanditiis rem libero optio autem qui, et incidunt ratione? Maxime iure, dicta sint, aspernatur ratione mollitia perferendis ipsa commodi itaque dolores in dolorem explicabo minima placeat?
+      Magni asperiores doloribus deleniti debitis optio quos mollitia eos et sequi, ea quo numquam doloremque, itaque adipisci veritatis quisquam dolore voluptates ipsum porro nostrum? Ducimus voluptatem eius repellat. Consectetur, voluptatum?
+      Expedita, quam dicta quos dolor nesciunt praesentium ipsam tenetur architecto similique repellat modi facilis rerum veritatis reiciendis, obcaecati magni assumenda quia velit omnis cum labore porro. Officiis asperiores mollitia debitis!
+      Ratione earum explicabo pariatur iusto, in distinctio iure at velit, dolor totam sequi magni. Suscipit blanditiis iste non voluptate unde quas voluptates, aspernatur, dicta sed nulla consequuntur, odio ullam eius!
+      Nesciunt ipsam neque iusto explicabo nam eveniet, error inventore excepturi animi laboriosam doloribus temporibus, id vero. Voluptas ex, iste distinctio, dolore nihil in ipsa necessitatibus vel doloremque ducimus dolores reprehenderit.</h6>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'HomeView',
